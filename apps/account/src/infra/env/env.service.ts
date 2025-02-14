8e8c0089bab2c2d97fc93e44592e96cbbf347eaa
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-02-14 01:29:44 -0300

    chore: add env config

---
+++ apps/account/src/infra/env/env.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common'
+// biome-ignore lint/style/useImportType: <explanation>
+import { ConfigService } from '@nestjs/config'
+import type { Env } from './env'
+
+@Injectable()
+export class EnvService {
+  constructor(private configService: ConfigService<Env, true>) {}
+
+  get<T extends keyof Env>(key: T) {
+    return this.configService.get(key, { infer: true })
+  }
+}

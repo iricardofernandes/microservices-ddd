91442299030b4b0435d28e3a6b7715f44bee68d7
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-02-16 23:24:02 -0300

    chore: move auth to infra folder

--- apps/account/src/infra/app.module.ts
+++ apps/account/src/infra/app.module.ts
@@ -1,4 +1,4 @@
-import { AuthModule } from '@/auth/auth.module'
+import { AuthModule } from '@/infra/auth/auth.module'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env/env'

92c13e4d09b33f9a9ab5141b09d32649767992f5
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-02-18 23:47:29 -0300

    chore: add complements in user entity

--- apps/account/src/domain/enterprise/entities/user.ts
+++ apps/account/src/domain/enterprise/entities/user.ts
@@ -1,26 +1,58 @@
-import { Entity, type UniqueEntityID } from '@ms/core'
+import { Entity, type Optional, type UniqueEntityID } from '@ms/core'
 
 export interface UserProps {
   name: string
   email: string
   password: string
+  createdAt: Date
+  updatedAt?: Date | null
 }
 
 export class User extends Entity<UserProps> {
+  set name(name: string) {
+    this.props.name = name
+    this.touch()
+  }
+
   get name() {
     return this.props.name
+  }
+
+  set email(email: string) {
+    this.props.email = email
+    this.touch()
   }
 
   get email() {
     return this.props.email
   }
 
+  set password(password: string) {
+    this.props.password = password
+    this.touch()
+  }
+
   get password() {
     return this.props.password
   }
 
-  static create(props: UserProps, id?: UniqueEntityID) {
-    const user = new User(props, id)
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  static create(props: Optional<UserProps, 'createdAt'>, id?: UniqueEntityID) {
+    const user = new User(
+      { ...props, createdAt: props.createdAt ?? new Date() },
+      id
+    )
 
     return user
   }

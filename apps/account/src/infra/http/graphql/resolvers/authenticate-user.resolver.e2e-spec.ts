84b914103513a4aa557565b73fe68b84cf49f4a9
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-02-24 23:44:52 -0300

    test: authenticate user test e2e

---
+++ apps/account/src/infra/http/graphql/resolvers/authenticate-user.resolver.e2e-spec.ts
@@ -0,0 +1,61 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+import { UserFactory } from 'test/factories/make-user'
+import { beforeAll } from 'vitest'
+
+describe('Register User Resolver (e2e)', () => {
+  let app: INestApplication
+  let userFactory: UserFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [UserFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    userFactory = moduleRef.get(UserFactory)
+
+    await app.init()
+  })
+
+  it('(Mutation) registerUser', async () => {
+    await userFactory.makeDrizzleUser({
+      email: 'johndoe@example.com',
+      password: await hash('123456', 8),
+    })
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `
+          mutation AuthenticateUser($data: AuthenticateUserInput!) {
+            authenticateUser(data: $data) {
+              email
+              password
+            }
+          }
+        `,
+        variables: {
+          data: {
+            email: 'johndoe@example.com',
+            password: '123456',
+          },
+        },
+      })
+      .expect(200)
+
+    const {
+      data: { registerUser: output },
+    } = response.body
+
+    expect(output).toMatchObject({
+      accessToken: expect.any(String),
+    })
+  })
+})
--- apps/account/test/factories/make-user.ts
+++ apps/account/test/factories/make-user.ts
@@ -1,6 +1,9 @@
 import { UniqueEntityID } from '@/core/unique-entity-id'
-import { User, type UserProps } from '@/domain/enterprise/entities/user'
+import { User, UserProps } from '@/domain/enterprise/entities/user'
+import { DrizzleService } from '@/infra/database/drizzle/drizzle.service'
+import { users } from '@/infra/database/drizzle/schema'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeUser(
   override: Partial<UserProps> = {},
@@ -17,4 +20,23 @@ export function makeUser(
   )
 
   return user
+}
+
+@Injectable()
+export class UserFactory {
+  constructor(private drizzle: DrizzleService) {}
+
+  async makeDrizzleUser(data: Partial<UserProps> = {}): Promise<User> {
+    const user = makeUser(data)
+
+    await this.drizzle.db.insert(users).values({
+      id: user.id.toString(),
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      createdAt: new Date(),
+    })
+
+    return user
+  }
 }

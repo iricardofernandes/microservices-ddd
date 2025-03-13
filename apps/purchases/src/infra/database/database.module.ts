9006ec88f3c19a0c419d60261e6769ce175275d8
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-03-12 23:28:12 -0300

    feat: add customers to database

--- apps/purchases/src/infra/database/database.module.ts
+++ apps/purchases/src/infra/database/database.module.ts
@@ -1,8 +1,10 @@
+import { CustomersRepository } from '@/domain/application/repositories/customers-repository'
 import { ProductsRepository } from '@/domain/application/repositories/products-repository'
 import { Module } from '@nestjs/common'
 import { EnvModule } from '../env/env.module'
 import { DrizzleService } from './drizzle/drizzle.service'
 import { DrizzleProductsRepository } from './drizzle/repositories/drizzle-products-repository'
+import { DrizzleCustomersRepository } from './drizzle/repositories/drizzle.customers.repository'
 
 @Module({
   imports: [EnvModule],
@@ -12,7 +14,11 @@ import { DrizzleProductsRepository } from './drizzle/repositories/drizzle-produc
       provide: ProductsRepository,
       useClass: DrizzleProductsRepository,
     },
+    {
+      provide: CustomersRepository,
+      useClass: DrizzleCustomersRepository,
+    },
   ],
-  exports: [DrizzleService, ProductsRepository],
+  exports: [DrizzleService, ProductsRepository, CustomersRepository],
 })
 export class DatabaseModule {}

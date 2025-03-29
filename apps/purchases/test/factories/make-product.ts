5b419e6e381c10e8e615281c30ea5ab44c547f49
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-03-28 23:33:31 -0300

    test: create product factorie

--- apps/purchases/test/factories/make-product.ts
+++ apps/purchases/test/factories/make-product.ts
@@ -1,6 +1,9 @@
 import { UniqueEntityID } from '@/core/unique-entity-id'
 import { Product, ProductProps } from '@/domain/enterprise/entities/product'
 import { Slug } from '@/domain/enterprise/entities/value-objects/slug'
+import type { DrizzleService } from '@/infra/database/drizzle/drizzle.service'
+import { products } from '@/infra/database/drizzle/schema'
+import { Injectable } from '@nestjs/common'
 
 export function makeProduct(
   override: Partial<ProductProps> = {},
@@ -16,4 +19,21 @@ export function makeProduct(
   )
 
   return product
+}
+
+@Injectable()
+export class ProductFactory {
+  constructor(private drizzle: DrizzleService) {}
+
+  async makeDrizzleProduct(data: Partial<ProductProps> = {}): Promise<Product> {
+    const product = makeProduct(data)
+
+    await this.drizzle.db.insert(products).values({
+      id: product.id.toString(),
+      authUserId: product.authUserId,
+      createdAt: new Date(),
+    })
+
+    return product
+  }
 }

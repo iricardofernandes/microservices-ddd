e32078d71572f77859c382d52e6c34cb95fefabd
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-03-30 23:02:30 -0300

    test: make purchase factory

--- apps/purchases/test/factories/make-purchase.ts
+++ apps/purchases/test/factories/make-purchase.ts
@@ -1,5 +1,8 @@
 import { UniqueEntityID } from '@/core/unique-entity-id'
 import { Purchase, PurchaseProps } from '@/domain/enterprise/entities/purchase'
+import type { DrizzleService } from '@/infra/database/drizzle/drizzle.service'
+import { purchases } from '@/infra/database/drizzle/schema'
+import { Injectable } from '@nestjs/common'
 
 export function makePurchase(
   override: Partial<PurchaseProps> = {},
@@ -16,4 +19,25 @@ export function makePurchase(
   )
 
   return purchase
+}
+
+@Injectable()
+export class PurchaseFactory {
+  constructor(private drizzle: DrizzleService) {}
+
+  async makeDrizzlePurchase(
+    data: Partial<PurchaseProps> = {}
+  ): Promise<Purchase> {
+    const purchase = makePurchase(data)
+
+    await this.drizzle.db.insert(purchases).values({
+      id: purchase.id.toString(),
+      customerId: purchase.customerId,
+      productId: purchase.productId,
+      status: purchase.status,
+      createdAt: new Date(),
+    })
+
+    return purchase
+  }
 }

fd5d687e27830f735c70484ad98550757140d496
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-03-31 22:45:56 -0300

    test: make course factory

--- apps/classroom/test/factories/make-course.ts
+++ apps/classroom/test/factories/make-course.ts
@@ -1,6 +1,9 @@
 import { UniqueEntityID } from '@/core/unique-entity-id'
 import { Course, CourseProps } from '@/domain/enterprise/entities/course'
 import { Slug } from '@/domain/enterprise/entities/value-objects/slug'
+import type { DrizzleService } from '@/infra/database/drizzle/drizzle.service'
+import { courses } from '@/infra/database/drizzle/schema'
+import { Injectable } from '@nestjs/common'
 
 export function makeCourse(
   override: Partial<CourseProps> = {},
@@ -16,4 +19,22 @@ export function makeCourse(
   )
 
   return course
+}
+
+@Injectable()
+export class CourseFactory {
+  constructor(private drizzle: DrizzleService) {}
+
+  async makeDrizzleCourse(data: Partial<CourseProps> = {}): Promise<Course> {
+    const course = makeCourse(data)
+
+    await this.drizzle.db.insert(courses).values({
+      id: course.id.toString(),
+      title: course.title,
+      slug: course.slug.toString(),
+      createdAt: new Date(),
+    })
+
+    return course
+  }
 }

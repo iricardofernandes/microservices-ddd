b971984253774268c8bfabc309a5c35f9d7319d8
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-04-20 23:31:42 -0300

    feat: create get enrollment by studentId

--- apps/classroom/src/domain/application/repositories/enrollments-repository.ts
+++ apps/classroom/src/domain/application/repositories/enrollments-repository.ts
@@ -1,5 +1,6 @@
 import { Enrollment } from '@/domain/enterprise/entities/enrollment'
 
 export abstract class EnrollmentsRepository {
+  abstract getEnrollmentsByStudentId(studentId: string): Promise<Enrollment[]>
   abstract create(enrollment: Enrollment): Promise<void>
 }

de88437669545fe9a32abf5ae642de9d1bd4f44a
Ricardo Fernandes <ricardo.fernandes.oliveira@outlook.com>
2025-05-02 22:33:10 -0300

    test: implement get enrollment interface

--- apps/classroom/test/repositories/in-memory-enrollments-repository.ts
+++ apps/classroom/test/repositories/in-memory-enrollments-repository.ts
@@ -4,6 +4,10 @@ import { Enrollment } from '@/domain/enterprise/entities/enrollment'
 export class InMemoryEnrollmentsRepository implements EnrollmentsRepository {
   public items: Enrollment[] = []
 
+  async getEnrollmentsByStudentId(studentId: string): Promise<Enrollment[]> {
+    throw new Error('Method not implemented.')
+  }
+
   async create(enrollment: Enrollment) {
     this.items.push(enrollment)
   }
